8621307679d571270b824fd3a3a20e0231392782
Fidel Ramos <fidelramos@gmail.com>
2013-07-25 18:24:15 +0200

    Personal settings for org-mode.

---
+++ personal/org.el
@@ -0,0 +1,22 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Org-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq org-directory "~/Docs/Org")
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+
+; add all *.org files in the org-directory defined above
+(setq org-agenda-files (directory-files org-directory 't "^[^#].*org$"))
+
+; use org-mode keys
+(setq org-disputed-keys (quote (([(shift up)] . [(meta shift up)])
+                                ([(shift down)] . [(meta shift down)])
+                                ([(shift left)] . [(meta shift left)])
+                                ([(shift right)] . [(meta shift right)])
+                                ([(control shift right)] . [(control meta shift right)])
+                                ([(control shift left)] . [(control meta shift left)])))
+      org-replace-disputed-keys t)
+
+; disable linum-mode in org buffers, it's too slow
+(add-hook 'org-mode-hook
+  '(lambda () (linum-mode 0)))
