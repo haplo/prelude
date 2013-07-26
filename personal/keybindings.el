8f31953598d41a8c9354d8d6fb396d6bb7f6113a
Fidel Ramos <fidelramos@gmail.com>
2013-07-26 09:55:43 +0200

    Use (kbd) in all personal keybindings.

--- personal/keybindings.el
+++ personal/keybindings.el
@@ -5,8 +5,8 @@
 (global-set-key (kbd "M--") 'hippie-expand)
 
 ; I hate minimize
-(global-set-key "\C-z" 'ignore)
-(global-set-key "\C-x\C-z" 'ignore)
+(global-set-key (kbd "C-z") 'ignore)
+(global-set-key (kbd "C-x C-z") 'ignore)
 
 ; quicker window splitting
 (global-set-key (kbd "M-1") 'delete-other-windows) ; was digit-argument
@@ -15,13 +15,12 @@
 (global-set-key (kbd "M-0") 'delete-window) ; was digit-argument
 (global-set-key (kbd "M-s") 'other-window) ; was center-linea
 
-; shortcuts for calc
-(global-set-key "\C-xc" 'calc)
-(global-set-key "\C-x\C-c" 'calc) ; was save-buffers-kill-terminal
-(global-set-key "\C-xC" 'full-calc)
+(global-set-key (kbd "C-x c") 'calc)
+(global-set-key (kbd "C-x C-c") 'calc) ; was save-buffers-kill-terminal
+(global-set-key (kbd "C-x C") 'full-calc)
 
 ; whitespace cleanup
-(global-set-key "\C-c\C-w" 'whitespace-cleanup)
+(global-set-key (kbd "C-c C-w") 'whitespace-cleanup)
 
 ; browse-kill-ring
 (global-set-key (kbd "C-x C-k") 'browse-kill-ring)
