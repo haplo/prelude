84ef6b54ad0faca502f15fdf846deafdea8f994c
Fidel Ramos <fidelramos@gmail.com>
2013-07-29 13:37:43 +0200

    Personal collection of Django snippets.

--- personal/snippets.el
+++ personal/snippets.el
@@ -2,8 +2,15 @@
 ;; Snippets
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; personal snippets dir
+(defvar prelude-personal-snippets-dir
+  (expand-file-name "snippets" prelude-personal-dir)
+  "This folder houses personal yasnippet bundles.")
+
 ;; load yasnippet
 (require 'yasnippet)
+(add-to-list 'yas-snippet-dirs (expand-file-name "django"
+                                                 prelude-personal-snippets-dir))
 (yas-global-mode 1)
 
 ;; term-mode does not play well with yasnippet
