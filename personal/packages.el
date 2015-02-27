805803399e90ba919438909e0e7297aa01670ac3
Fidel Ramos <fidelramos@gmail.com>
2015-02-27 10:57:20 -0800

    Add visual-regexp-steroids.
    Python regexps FTW.

--- personal/packages.el
+++ personal/packages.el
@@ -2,4 +2,5 @@
 ;; Auto-installed packages
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(prelude-require-packages '(ag solarized-theme yasnippet))
+(prelude-require-packages '(ag pip-requirements solarized-theme
+                            visual-regexp-steroids yasnippet))
---
+++ personal/visual-regexp-steroids.el
@@ -0,0 +1,12 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  visual-regexp-steroids customization
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'visual-regexp-steroids)
+
+(define-key global-map (kbd "C-c C-r") 'vr/replace)
+(define-key global-map (kbd "C-c C-q") 'vr/query-replace)
+
+; use visual-regexp-steroids's isearch instead of built-in regexp isearch
+(define-key esc-map (kbd "C-r") 'vr/isearch-backward) ;; C-M-r
+(define-key esc-map (kbd "C-s") 'vr/isearch-forward) ;; C-M-s
