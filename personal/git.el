1ee767f1bbec6107a98cda85cd6f256d7865c882
Fidel Ramos <fidelramos@gmail.com>
2015-10-25 16:08:21 +0000

    Magit configuration

--- personal/git.el
+++ personal/git.el
@@ -2,5 +2,16 @@
 ;; Git settings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; use Magit WIP minor mode to save changing files
+(setq magit-wip-after-apply-mode' t)
+(setq magit-wip-after-save-mode' t)
+(setq magit-wip-before-change-mode' t)
+
 ;; path to my root code dir, so I can do C-x g from anywhere
-(setq magit-repo-dirs '("~/Code/"))
+(setq magit-repository-directories '("~/Code/"))
+
+;; create a local tracking branch when visiting a remote branch
+(setq magit-visit-ref-create t)
+
+;; don't ask for confirmation when pushing branches
+(setq magit-push-always-verify nil)
