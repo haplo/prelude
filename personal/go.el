7d93465f9443e61cc0c5383f8e83c9fb110ffd8f
Fidel Ramos <f@fidelramos.net>
2019-08-04 11:24:41 +0000

    Use gogetdoc to display go documentation at point

---
+++ personal/go.el
@@ -0,0 +1,7 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Go configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; prefer gogetdoc over godoc and godef
+; install with go get github.com/zmb3/gogetdoc
+(setq godoc-at-point-function 'godoc-gogetdoc)
