bfa88b1b7db556004b59949bbfbc12395c2ec80e
Fidel Ramos <f@fidelramos.net>
2019-08-04 11:58:28 +0000

    Install godoctor package

--- personal/packages.el
+++ personal/packages.el
@@ -2,5 +2,9 @@
 ;; Auto-installed packages
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(prelude-require-packages '(ag pip-requirements solarized-theme
-                            visual-regexp-steroids yasnippet))
+(prelude-require-packages '(ag
+                            godoctor
+                            pip-requirements
+                            solarized-theme
+                            visual-regexp-steroids
+                            yasnippet))
