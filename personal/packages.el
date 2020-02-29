60d317f159a8665a75bf29a9be3eb1efc72f7564
Fidel Ramos <f@fidelramos.net>
2020-02-29 22:49:56 +0000

    Load svelte-mode if available

--- personal/packages.el
+++ personal/packages.el
@@ -11,3 +11,9 @@
                             solarized-theme
                             visual-regexp-steroids
                             yasnippet))
+
+; load svelte-mode, but must be checked out from github
+; git clone https://github.com/leafOfTree/svelte-mode --depth 1 ~/Code/svelte-mode
+(if (file-directory-p "~/Code/svelte-mode")
+    (progn (add-to-list 'load-path "~/Code/svelte-mode")
+           (require 'svelte-mode)))
