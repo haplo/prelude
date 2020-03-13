fe48f435897e039c902e08132791fc0778632e70
Fidel Ramos <f@fidelramos.net>
2020-03-13 10:49:33 +0100

    Detect QubesOS and use its GPG client wrapper

--- personal/gpg.el
+++ personal/gpg.el
@@ -2,11 +2,13 @@
 ;; GPG/PGP configuration
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; Use QubesOS' wrapper for split GPG
-(require 'epg-config)
-(setq epg-gpg-program "/usr/bin/qubes-gpg-client-wrapper")
-(push (cons 'OpenPGP (epg-config--make-gpg-configuration epg-gpg-program))
-      epg--configurations)
+; Detect QubesOS and use its wrapper for split GPG
+(if (file-exists-p "/usr/bin/qubes-gpg-client-wrapper")
+    (progn
+      (require 'epg-config)
+      (setq epg-gpg-program "/usr/bin/qubes-gpg-client-wrapper")
+      (push (cons 'OpenPGP (epg-config--make-gpg-configuration epg-gpg-program))
+            epg--configurations)))
 
 ; Play nice with GPG signatures in Emacs packages
 (setq package-gnupghome-dir nil)
