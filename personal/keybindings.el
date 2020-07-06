3cd6ef1512fa704194d51cd2bffb5d79aa83b44c
Fidel Ramos <f@fidelramos.net>
2020-07-06 19:41:29 +0200

    Use custom global minor mode for keybindings
    This solves the problem that some keybindings are set by major modes and
    not directly remappable when using global-set-key. By using a custom minor
    mode the defined keymap can have precedence over everything else.
    
    Solution taken from
    https://emacs.stackexchange.com/questions/352/how-to-override-major-mode-bindings/358#358

--- personal/keybindings.el
+++ personal/keybindings.el
@@ -2,33 +2,40 @@
 ;; Keybindings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; M-/ isn't easily available in my Spanish keyboard
-(global-set-key (kbd "M--") 'hippie-expand)
+;; M-- is in the same place as M-/ in a Spanish keyboard
+(define-key my-mode-map (kbd "M--") 'hippie-expand)
 
 ;; I hate minimize
-(global-set-key (kbd "C-z") 'ignore)
-(global-set-key (kbd "C-x C-z") 'ignore)
+(define-key my-mode-map (kbd "C-z") 'ignore)
+(define-key my-mode-map (kbd "C-x C-z") 'ignore)
 
 ;; quicker window splitting
-(global-set-key (kbd "M-1") 'delete-other-windows) ; was digit-argument
-(global-set-key (kbd "M-2") 'split-window-vertically) ; was digit-argument
-(global-set-key (kbd "M-3") (lambda () (interactive) (funcall 'split-window-horizontally (+ fill-column 4)))) ; was digit-argument
-(global-set-key (kbd "M-0") 'delete-window) ; was digit-argument
-(global-set-key (kbd "M-s") 'other-window) ; was center-linea
+(define-key my-mode-map (kbd "M-1") 'delete-other-windows) ; was digit-argument
+(define-key my-mode-map (kbd "M-2") 'split-window-vertically) ; was digit-argument
+(define-key my-mode-map (kbd "M-3") (lambda () (interactive) (funcall 'split-window-horizontally (+ fill-column 4)))) ; was digit-argument
+(define-key my-mode-map (kbd "M-0") 'delete-window) ; was digit-argument
+(define-key my-mode-map (kbd "M-s") 'other-window) ; was center-linea
 
 ;; quick access to calculator
-(global-set-key (kbd "C-x c") 'calc)
-(global-set-key (kbd "C-x C-c") 'calc) ; was save-buffers-kill-terminal
-(global-set-key (kbd "C-x C") 'full-calc)
+(define-key my-mode-map (kbd "C-x c") 'calc)
+(define-key my-mode-map (kbd "C-x C-c") 'calc) ; was save-buffers-kill-terminal
+(define-key my-mode-map (kbd "C-x C") 'full-calc)
 
 ;; whitespace cleanup
-(global-set-key (kbd "C-c C-w") 'whitespace-cleanup)
+(define-key my-mode-map (kbd "C-c C-w") 'whitespace-cleanup)
 
 ;; browse-kill-ring
-(global-set-key (kbd "C-x C-k") 'browse-kill-ring)
+(define-key my-mode-map (kbd "C-x C-k") 'browse-kill-ring)
 
 ;; Org-mode keybindings
-(global-set-key (kbd "C-c c") 'org-capture)
+(define-key my-mode-map (kbd "C-c c") 'org-capture)
+
+;; ivy, counsel, swiper
+(define-key my-mode-map (kbd "C-c v") 'ivy-push-view)
+(define-key my-mode-map (kbd "C-c V") 'ivy-pop-view)
+(define-key my-mode-map (kbd "C-c L") 'counsel-git-log)
+(define-key my-mode-map (kbd "C-c k") 'counsel-rg)
+(define-key my-mode-map (kbd "C-c C-o") 'counsel-org-goto-all)
 
 ;; Python keybindings
 (eval-after-load 'anaconda-mode
