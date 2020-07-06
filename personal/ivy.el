9d4c54f1cb132f2f81b5b14faf3a0b741f0000a0
Fidel Ramos <f@fidelramos.net>
2020-07-06 12:25:19 +0200

    Case-insensitive search in counsel-git-log

--- personal/ivy.el
+++ personal/ivy.el
@@ -5,3 +5,6 @@
 
 ;; show more candidates
 (setq ivy-height 20)
+
+;; case-insensitive search when running counsel-git-log
+(setq counsel-git-log-cmd "GIT_PAGER=cat git log --no-color -i --grep '%s'")
