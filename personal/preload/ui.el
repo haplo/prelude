dab87293b8e67d050b0e28e232fc36a7371df642
Fidel Ramos <f@fidelramos.net>
2020-11-04 10:04:57 +0100

    Disable menu bar and line numbers
    Prelude now enables them by default. Oh the times they are a-changin'!

---
+++ personal/preload/ui.el
@@ -0,0 +1,6 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; UI tweaks
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; disable menu bar and line numbers (see doc/configuration.md)
+(setq prelude-minimalistic-ui t)
