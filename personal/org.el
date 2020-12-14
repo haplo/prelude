290fa5ba10e2115c01bf192d8c038329ce226d53
Fidel Ramos <f@fidelramos.net>
2020-12-14 10:10:19 +0100

    Properly configure org-disputed-keys
    This configuration needs to be set *before* org-mode loads, so it needs to
    go into `preload`.

--- personal/org.el
+++ personal/org.el
@@ -43,15 +43,6 @@
          "** [[%^{PR URL}][%^{PR description}]]" :clock-in t :clock-resume t)
         ))
 
-;; use org-mode keys
-(setq org-disputed-keys (quote (([(shift up)] . [(meta shift up)])
-                                ([(shift down)] . [(meta shift down)])
-                                ([(shift left)] . [(meta shift left)])
-                                ([(shift right)] . [(meta shift right)])
-                                ([(control shift right)] . [(control meta shift right)])
-                                ([(control shift left)] . [(control meta shift left)])))
-      org-replace-disputed-keys t)
-
 ;; disable linum-mode in org buffers, it's too slow
 (add-hook 'org-mode-hook
   '(lambda () (linum-mode 0)))
---
+++ personal/preload/org.el
@@ -0,0 +1,6 @@
+;; use org-mode keys
+(setq org-disputed-keys (quote (([(shift up)] . [(super shift up)])
+                                ([(shift down)] . [(super shift down)])
+                                ([(shift left)] . [(super shift left)])
+                                ([(shift right)] . [(super shift right)])))
+      org-replace-disputed-keys t)
