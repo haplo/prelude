b5d5f4e52fc139695fa7cd1adc7a78f82c65ae43
Fidel Ramos <f@fidelramos.net>
2020-12-22 11:43:31 +0100

    Attempt to get pyls to use flake8
    Doesn't seem to work just yet, but for some reason this configuration
    fixes pyls integration with the virtualenv activated by pyvenv. ¯\_ (ツ)_/¯

--- personal/python.el
+++ personal/python.el
@@ -16,11 +16,19 @@
 (setq pyvenv-mode-line-indicator
       '(pyvenv-virtual-env-name ("[venv:" pyvenv-virtual-env-name "] ")))
 
+;; prefer flake8 over pyflakes in pyls
+;; https://vxlabs.com/2018/06/08/python-language-server-with-emacs-and-lsp-mode/
+(defun lsp-python-use-flake8 ()
+  (let ((lsp-cfg `(:pyls (:configurationSources ("flake8")))))
+    ;; TODO: check lsp--cur-workspace here to decide per server / project
+    (lsp--set-configuration lsp-cfg)))
+
 (defun my-python-config ()
   "My personal configuration for python-mode"
   (subword-mode +1)
   (pyvenv-mode +1)
   (auto-virtualenv-set-virtualenv)
+  (lsp-python-use-flake8)
   (lsp)
   (python-black-on-save-mode)
   )
