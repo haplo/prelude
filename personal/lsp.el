f12bfcd13b6f08b98f910cd623957dce999288a3
Fidel Ramos <f@fidelramos.net>
2021-02-04 15:52:17 +0100

    lsp-enable-imenu

--- personal/lsp.el
+++ personal/lsp.el
@@ -1,6 +1,8 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; LSP and lsp-mode configuration
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Language-specific configuration in their respective files, e.g. python.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; display documentation by the cursor, not at the top of the window
 (setq-default lsp-ui-doc-position 'at-point)
@@ -12,3 +14,5 @@
 ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
 (setq gc-cons-threshold 100000000)
 (setq read-process-output-max (* 1024 1024)) ;; 1 MiB
+
+(add-hook 'lsp-after-open-hook 'lsp-enable-imenu)
